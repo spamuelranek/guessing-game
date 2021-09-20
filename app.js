43abb489816df3794023a35a6be1a15554171ce2
Samuel Panek <samuelrpanek@gmail.com>
2021-09-20 08:52:00 -0700

    adds funtions to all the variables

--- app.js
+++ app.js
@@ -4,45 +4,62 @@ let userAnswer = prompt("What is your name?");
 
 function userName() {
     alert("Welcome " + userAnswer + ". So glad you could join us today.");
+    // window.addEventListener('DOMContentLoaded', [function to run]);
     document.write('<p id="title" > "Welcome "' + userAnswer + '"!</p>"');
 }
 
 
+function questions (){
+    let questionWhere = "Does Sam live in Cleveland?";
+    let questionAge = "Is Sam older than Playstation?";
+    let questionSport = "Has Sam ever played a team sport?";
+    let questionVehicle = "Can Sam drive a manual vehicle?";
+    let questionRead = "Does sam like to read mostly Romance Novels?";
+    let questionTotal = [questionWhere, questionAge, questionSport, questionVehicle, questionRead];
+    return questionTotal;
+}
 
-let questionWhere = "Does Sam live in Cleveland?";
-let questionAge = "Is Sam older than Playstation?";
-let questionSport = "Has Sam ever played a team sport?";
-let questionVehicle = "Can Sam drive a manual vehicle?";
-let questionRead = "Does sam like to read mostly Romance Novels?";
-let questionTotal = [questionWhere, questionAge, questionSport, questionVehicle, questionRead];
-let alertWhereCorrect = "That's Correct. Sam lives just outside of Seattle";
-let alertAgeCorrect = "That is correct. Sam is one year older than the Playstation Console";
-let alertSportCorrect = "That is correct. Sam has played several and his favorite is Soccer";
-let alertDriveCorrect = "That is correct. In fact his first car was a baby blue Kia Rio. It was a manual.";
-let alertReadCorrect = "That is correct. Sam mostly reads Science Fiction ";
-let alertCorrecttotal = [alertWhereCorrect, alertAgeCorrect, alertSportCorrect, alertDriveCorrect, alertReadCorrect];
-let alertWhereIncorrect = "I'm sorry that is incorrect. Sam currently resides just outside of Seattle";
-let alertAgeIncorrect = "That is incorrect. Sam is one year older than the Playstiation Console";
-let alertSportIncorrect = "That's incorrect. Sam has played several team sports throughout his life";
-let alertDriveIncorrect = "That is incorrect. Sam learned to drive on a manual";
-let alertReadIncorrect = "Alas, Sam does mostly read Sci-Fi. He enjoys others but that is his mainstream";
-let alertIncorrecttotal = [alertWhereIncorrect, alertAgeIncorrect, alertSportIncorrect, alertDriveIncorrect, alertReadIncorrect];
-let grandArray = [questionTotal, alertCorrecttotal, alertIncorrecttotal]
+console.log(questions())
+
+function alertCorrect (){
+    let alertWhereCorrect = "That's Correct. Sam lives just outside of Seattle";
+    let alertAgeCorrect = "That is correct. Sam is one year older than the Playstation Console";
+    let alertSportCorrect = "That is correct. Sam has played several and his favorite is Soccer";
+    let alertDriveCorrect = "That is correct. In fact his first car was a baby blue Kia Rio. It was a manual.";
+    let alertReadCorrect = "That is correct. Sam mostly reads Science Fiction ";
+    let alertCorrecttotal = [alertWhereCorrect, alertAgeCorrect, alertSportCorrect, alertDriveCorrect, alertReadCorrect];
+    return alertCorrecttotal;
+}
+
+function alertIncorrect (){
+    let alertWhereIncorrect = "I'm sorry that is incorrect. Sam currently resides just outside of Seattle";
+    let alertAgeIncorrect = "That is incorrect. Sam is one year older than the Playstiation Console";
+    let alertSportIncorrect = "That's incorrect. Sam has played several team sports throughout his life";
+    let alertDriveIncorrect = "That is incorrect. Sam learned to drive on a manual";
+    let alertReadIncorrect = "Alas, Sam does mostly read Sci-Fi. He enjoys others but that is his mainstream";
+    let alertIncorrecttotal = [alertWhereIncorrect, alertAgeIncorrect, alertSportIncorrect, alertDriveIncorrect, alertReadIncorrect];
+    return alertIncorrecttotal;
+}
+
+// let grandArray = [questionTotal, alertCorrecttotal, alertIncorrecttotal]
 let incorrectArray =["yes","no","no","no","yes"]
 let correctArray =["no","yes","yes","yes","no",]
 let gamePoints = 0;
 
 
 
-function knowSam() {
+function knowSam(questions, alertCorrect, alertIncorrect) {
+    questions ();
+    alertCorrect ();
+    alertIncorrect ();
     for (let guess = 0; guess<5; guess++){
-    let ansWer = prompt(questionTotal[guess]);
-    if(ansWer.toLowerCase() == incorrectArray[guess]){
+        let ansWer = prompt(questions[guess]);
+        if(ansWer.toLowerCase() == incorrectArray[guess]){
     // console.log( "I'm sorry that is incorrect. Sam currently resides just outside of Seattle")
-    alert(alertIncorrecttotal[guess]);
+        alert(alertIncorrect[guess]);
     }  else if (ansWer.toLowerCase() == correctArray[guess]){
     // console.log("That's Correct. Sam lives just outside of Seattle")
-        alert(alertCorrecttotal[guess]);
+        alert(alertCorrect[guess]);
         gamePoints++;
     }
 }
